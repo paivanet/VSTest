20160e4352baadf90c3aea02c6c48567ac2e5048
cocoPaiva <jpaiva_peru@yahoo.es>
2017-11-27 03:05:41 -0500

    last

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,12 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //inisializar
+            //hola2 
+
+        }
     }
 }
